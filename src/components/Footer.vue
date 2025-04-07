abf03d4036b4ffcf995e73046a2a67a4c702d0e1
thianah <ekundayo6789@gmail.com>
2025-04-07 16:06:41 +0100

    Fix App.vue import and rebuild project

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="footer bg-slate-900 text-blue-100 mt-25 text-center p-4 ">
+  <footer class="footer bg-slate-900 text-blue-100 mt-40 text-center p-4 ">
     <p>&copy; 2024 Thianah Todo App. All rights reserved.</p>
   </footer>
 </template>
